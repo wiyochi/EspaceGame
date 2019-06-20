a4c39ff252f76d265acb992318d4bd134696b2b8
wiyochi <mat.arquilliere@gmail.com>
2019-06-21 00:24:45 +0200

    Tests rapidjson
    Premiers tests de rapidjson (pour voir le fonctionnement de récupération des données des json)

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,10 +1,45 @@
+#include <iostream>
+#include <fstream>
 #include <SFML/Graphics.hpp>
+#include <rapidjson/document.h>
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
+
+	std::ifstream file("resources/saves/saveTest.json", std::ios::in);
+	std::string str;
+
+	if (file)
+	{
+		char c;
+		while (file.get(c))
+			str.push_back(c);
+
+		file.close();
+	}
+	else
+	{
+		std::cout << "Erreur load file" << std::endl;
+	}
+
+	const char* json = str.c_str();
+	rapidjson::Document d;
+	d.Parse(json);
+
+	rapidjson::Value& s = d["mine"]["machines"]["x"];
+	for (rapidjson::SizeType i = 0; i < s.Size(); i++)
+		printf("a[%d] = %d\n", i, s[i].GetInt());
+
+	rapidjson::StringBuffer buffer;
+	rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+	d.Accept(writer);
+
+	std::cout << buffer.GetString() << std::endl;
 
 	while (window.isOpen())
 	{
