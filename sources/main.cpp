375477518f2eb03e912ebd2cc03ecdb455b95149
wiyochi <mat.arquilliere@gmail.com>
2019-08-07 21:23:04 +0200

    Début de panel
    Ya rien mais c'est pour bien set la branche

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,86 +1,22 @@
 #include <iostream>
-#include <fstream>
 #include <SFML/Graphics.hpp>
-#include "graphics/UI/PoleButton.hpp"
-#include <string>
-#include "graphics/UI/Case.hpp"
-#include <random>
-#include <time.h>
-#include "graphics/utils/TextureManager.hpp"
-#include "JSonInterface/Loader.hpp"
-#include "JSonInterface/Writer.hpp"
-#include "map/Grid.hpp"
-#include "skillTree/Tree.hpp"
-#include "skillTree/Node.hpp"
 
-typedef enum {
-	MAIN_MENU, POLE_1, POLE_2, POLE_3, POLE_4,
-} GAME_STATE;
-
-GAME_STATE STATE = MAIN_MENU;
-
-void loading() {
-	loadTexture("DIRT", "resources/textures/tiles/dirt.png");
-	loadTexture("STONE", "resources/textures/tiles/stone.png");
-	loadTexture("WATER", "resources/textures/tiles/water.png");
-	loadTexture("GRASS", "resources/textures/tiles/grass.png");
-}
-
-void stoping() {
-	freeTextureManager();
-}
-
-int main() {
-
-	loading();
+int main()
+{
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
 
-	PoleButton pole1(0, window);
-	PoleButton pole2(1, window);
-	PoleButton pole3(2, window);
-	PoleButton pole4(3, window);
-
-	//sf::View _view(sf::FloatRect(0, 0, 70000, 1700));
-	//window.setView(_view);
+	sf::RectangleShape rs(sf::Vector2f(200.f, 200.f));
+	rs.setPosition(sf::Vector2f(500.f, 500.f));
+	rs.setFillColor(sf::Color::Blue);
 
 	// View (d�bug)
 	sf::View v;
-	float viewSpeed = 0.5f;
+	float viewSpeed = 2.f;
 
 	// Game Time
 	sf::Clock clock;
 	sf::Time elapsedTime;
 	int	deltaTime;
-
-	// Poles declaration
-	Grid* poles[4] = { nullptr };
-
-	Tree* t = nullptr;
-
-	// Load game objetcs
-	Loader::loadItems("resources/item/items.json");
-	Loader::loadSave("resources/save/saveTest.json", poles);
-	if (Loader::loadSkillTree("resources/skillTree/test.json", &t))
-	{
-		t->setPosition(sf::Vector2f(900.f, 20.f));
-		t->initLinks();
-	}
-
-	bool keyPressed_S = false;
-
-	for (unsigned int i = 0; i < Item::items.size(); i++)
-	{
-		std::cout << "Item " << i << " : " << *(Item::items[i]) << std::endl;
-	}
-
-	for (size_t i = 0; i < 4; ++i)
-	{
-		std::cout << "Pole " << poles[i]->getName() << std::endl;
-		for (const auto machine : poles[i]->getMachines())
-		{
-			std::cout << *(machine) << std::endl;
-		}
-	}
 
 	while (window.isOpen())
 	{
@@ -109,23 +45,6 @@ int main() {
 				if(event.key.code == sf::Keyboard::Escape)
 					window.close();
 				break;
-            case sf::Event::MouseButtonPressed:
-				float x = window.mapPixelToCoords(sf::Mouse::getPosition(window)).x;
-				float y = window.mapPixelToCoords(sf::Mouse::getPosition(window)).y;
-				std::cout << "SREEN{" << event.mouseButton.x << ";" << event.mouseButton.y << "}   ||    GAME{" << x << ";" << y << "}" << std::endl;
-				switch (STATE) 
-                {
-					case MAIN_MENU:
-						if (pole1.contains(x, y)) STATE = POLE_1;
-						if (pole2.contains(x, y)) STATE = POLE_2;
-						if (pole3.contains(x, y)) STATE = POLE_3;
-						if (pole4.contains(x, y)) STATE = POLE_4;
-						break;
-					case POLE_1 ... POLE_4:
-						STATE = MAIN_MENU;
-                        break;
-				}
-				break;
 			}
 		}
 
@@ -133,18 +52,6 @@ int main() {
 		elapsedTime = clock.getElapsedTime();
 		clock.restart();
 		deltaTime = elapsedTime.asMilliseconds();
-
-		// S key to save actual poles
-		if(sf::Keyboard::isKeyPressed(sf::Keyboard::S) && !keyPressed_S)
-		{
-			keyPressed_S = true;
-			std::cout << "Sauvegarde..." << std::endl;
-			Writer::save("resources/save/testWriter.json", poles);
-		}
-		else if(!sf::Keyboard::isKeyPressed(sf::Keyboard::S) && keyPressed_S)
-		{
-			keyPressed_S = false;
-		}
 
 		// Arrows to move camera
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
@@ -157,77 +64,15 @@ int main() {
 			v.move(viewSpeed * deltaTime, 0);
 
 		// Updates
-
-		// On met en stand-by pour l'instant à cause du chevauchement
-		//t->update(window);
-		//for (const auto pole : poles)
-		//	pole->update(window);
-		//window.setView(v);
+		window.setView(v);
 
 		window.clear();
-		switch (STATE) {
-			case MAIN_MENU:
-				pole1.setMapMode(Map::SPLITSCREEN);
-				pole2.setMapMode(Map::SPLITSCREEN);
-				pole3.setMapMode(Map::SPLITSCREEN);
-				pole4.setMapMode(Map::SPLITSCREEN);
-				break;
-			case POLE_1:
-				pole1.setMapMode(Map::FULLSCREEN);
-				pole2.setMapMode(Map::NO_SCREEN);
-				pole3.setMapMode(Map::NO_SCREEN);
-				pole4.setMapMode(Map::NO_SCREEN);
-				break;
-			case POLE_2:
-				pole1.setMapMode(Map::NO_SCREEN);
-				pole2.setMapMode(Map::FULLSCREEN);
-				pole3.setMapMode(Map::NO_SCREEN);
-				pole4.setMapMode(Map::NO_SCREEN);
-				break;
-			case POLE_3:
-				pole1.setMapMode(Map::NO_SCREEN);
-				pole2.setMapMode(Map::NO_SCREEN);
-				pole3.setMapMode(Map::FULLSCREEN);
-				pole4.setMapMode(Map::NO_SCREEN);
-				break;
-			case POLE_4:
-				pole1.setMapMode(Map::NO_SCREEN);
-				pole2.setMapMode(Map::NO_SCREEN);
-				pole3.setMapMode(Map::NO_SCREEN);
-				pole4.setMapMode(Map::FULLSCREEN);
-				break;
-			default:
-				break;
-		}
 
 		// Draws
-		pole1.draw();
-		pole2.draw();
-		pole3.draw();
-		pole4.draw();
-
-		// On met en stand-by pour l'instant à cause du chevauchement
-		//for (size_t i = 0; i < 4; i++)
-		//	window.draw(*(poles[i]));
-		//window.draw(*t);
+		window.draw(rs);
 
 		window.display();
 	}
-
-	stoping();
-
-	// Poles and machines memory release
-	for(size_t i = 0; i < 4; i++)
-	{
-		if(poles[i] != nullptr)
-			delete poles[i];
-	}
-
-	// Items memory release
-	Item::deleteItems();
-	
-	// Tree test memory release
-	delete t;
 
 	return 0;
 }
